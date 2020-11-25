76ef0e445823291afc2ccdd6c87e55596ea9f769
Matt Disalov <61394693+mattdisalov@users.noreply.github.com>
2020-11-24 21:18:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { green, orange } from '@material-ui/core/colors';
+import 'fontsource-roboto';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #333, #000',
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: 'white',
+    padding: '5 30px',
+  }
+})
+
+const theme = createMuiTheme({
+  typography: {
+    h2: {
+      fontSize: 36,
+    }
+  },
+})
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>
+}
+
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true)
+  return(
+    <FormControlLabel 
+      control ={<Checkbox
+        checked={checked} 
+        icon={<DeleteIcon />}
+        checkedIcon={<SaveIcon />}
+        onChange={(e)=>setChecked(e.target.checked)}
+        inputProps={{
+          'aria-label': 'secondary-checkbox'
+        }}
+      />}
+      label="Testing Chechbox"
+    />
+  )
+}
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="xs">
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AppBar color="secondary">
+          <ToolBar>
+            <IconButton>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">
+              MUI Themeing
+            </Typography>
+            <Button>
+              Login
+            </Button>
+          </ToolBar>
+        </AppBar>
+        <Typography variant='h2' component="div">
+          Welcome to MUI
+        </Typography>
+        <Typography variant='subtitle1'>
+          Learn how to use material ui
+        </Typography>
+        <ButtonStyled />
+        
+        <Grid container spacing={2} justify="center">
+          <Grid item xs={12} sm={6}>
+            <Paper style={{height:75, width:'100%',}} />
+          </Grid>
+          <Grid item xs={3}>
+            <Paper style={{height:75, width:'100%',}} />
+          </Grid>
+          <Grid item xs={3}>
+            <Paper style={{height:75, width:'100%',}} />
+          </Grid>
+        </Grid>
+
+        <CheckboxExample />
+        <ButtonGroup variant="contained" color="primary">
+          <Button 
+            startIcon={<SaveIcon />} >
+            Save
+          </Button>
+          <Button 
+            startIcon={<DeleteIcon />} >
+            Discard
+          </Button>
+        </ButtonGroup>
+        <img src={logo} className="App-logo" alt="logo" />  
       </header>
     </div>
+    </Container>
+    </ThemeProvider>
   );
 }
 
